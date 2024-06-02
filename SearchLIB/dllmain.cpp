ce73fee8ad81bc796467552b5e71192be92e883f
ronetel <144216054+ronetel@users.noreply.github.com>
2024-06-02 23:35:43 +0300

    a

---
+++ SearchLIB/dllmain.cpp
@@ -0,0 +1,36 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+
+extern "C" __declspec(dllexport) bool prikol(const char* str, const char* chars) {
+    bool yes = true;
+
+    for (int i = 0; chars[i] != '\0' && yes; ++i) {
+        yes = false;
+
+        for (int j = 0; str[j] != '\0'; ++j) {
+            if (chars[i] == str[j]) {
+                yes = true;
+                break;
+            }
+        }
+    }
+
+    return yes;
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
---
+++ new 8gh/new 8gh.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+
+using namespace std; 
+typedef bool (*prikol)(const char*, const char*);
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+
+    HINSTANCE lib;
+    prikol address;
+
+    string input;
+    string findChar;
+
+    cout << "Введите слово/фразу: ";
+    getline(cin, input);
+
+    cout << "Введите буковки для поиска: ";
+    getline(cin, findChar);
+
+       lib = LoadLibrary(TEXT("SearchLIB.dll"));
+    if (lib != NULL) {
+        address = (prikol)GetProcAddress(lib, "prikol");
+
+        if (address != NULL) {
+            bool found = address(input.c_str(), findChar.c_str());
+
+            if (found) {
+                cout << "Все символы найдены." << endl;
+            }
+            else {
+                cout << "Не все символы найдены." << endl;
+            }
+        }
+        else {
+            cerr << "Функция не найдена." << endl;
+        }
+        FreeLibrary(lib);
+    }
+    else {
+        cerr << "DLL не была загружена." << endl;
+    }
+
+    return 0;
+}
